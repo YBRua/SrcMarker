a52f165ddefbb436ede6a0bc53ca4811ef66112a
YBRua <ybirua@sjtu.edu.cn>
2024-03-02 13:21:21 +0800

    Fixed dataset filtering - Updated to stringifier APIs

--- dataset_filter.py
+++ dataset_filter.py
@@ -5,7 +5,12 @@ import json
 import tree_sitter
 from tqdm import tqdm
 from collections import Counter
-from mutable_tree.adaptors import JavaAdaptor, CppAdaptor
+from mutable_tree.adaptors import JavaAdaptor, CppAdaptor, JavaScriptAdaptor
+from mutable_tree.stringifiers import (
+    JavaStringifier,
+    CppStringifier,
+    JavaScriptStringifier,
+)
 from typing import List
 
 
@@ -42,28 +47,44 @@ def remove_comments(source: str):
     return "\n".join(temp)
 
 
-def get_java_function_root(root: tree_sitter.Node):
+def _get_java_function_root(root: tree_sitter.Node) -> tree_sitter.Node:
     assert root.type == "program"
     class_decl_node = root.children[0]
     assert class_decl_node.type == "class_declaration"
     class_body_node = class_decl_node.children[3]
     assert class_body_node.type == "class_body"
     func_root_node = class_body_node.children[1]
-    assert func_root_node.type == "method_declaration"
+    assert func_root_node.type == "method_declaration", func_root_node.type
     return func_root_node
 
 
-def get_cpp_function_root(root: tree_sitter.Node):
+def _get_cpp_function_root(root: tree_sitter.Node) -> tree_sitter.Node:
     assert root.type == "translation_unit"
     func_root_node = root.children[0]
-    assert func_root_node.type == "function_definition", func_root_node.type
+    assert func_root_node.type == "function_definition"
     return func_root_node
+
+
+def _get_javascript_function_root(root: tree_sitter.Node) -> tree_sitter.Node:
+    assert root.type == "program"
+    func_root_node = root.children[0]
+
+    if func_root_node.type == "function_declaration":
+        return func_root_node
+    elif func_root_node.type == "expression_statement":
+        func_root_node = func_root_node.children[0]
+        assert func_root_node.type == "function", func_root_node.type
+        return func_root_node
+    elif func_root_node.type == "generator_function_declaration":
+        return func_root_node
+    else:
+        raise RuntimeError(f"Unexpected root node type: {func_root_node.type}")
 
 
 def compare_tokens(old_tokens: List[str], new_tokens: List[str]):
     if len(old_tokens) != len(new_tokens):
         return False
-    for old_token, new_token in zip(old_tokens, new_tokens):
+    for i, (old_token, new_token) in enumerate(zip(old_tokens, new_tokens)):
         if old_token != new_token:
             return False
     return True
@@ -81,19 +102,30 @@ def function_round_trip(parser: tree_sitter.Parser, code: str, lang: str):
 
     assert not tree.root_node.has_error
     if lang == "java":
-        func_root = get_java_function_root(tree.root_node)
+        func_root = _get_java_function_root(tree.root_node)
         try:
             mutable_root = JavaAdaptor.convert_function_declaration(func_root)
         except Exception as e:
             return (False, str(e))
-    else:
-        func_root = get_cpp_function_root(tree.root_node)
+        stringifier = JavaStringifier()
+    elif lang == "cpp":
+        func_root = _get_cpp_function_root(tree.root_node)
         try:
             mutable_root = CppAdaptor.convert_function_definition(func_root)
         except Exception as e:
             return (False, str(e))
+        stringifier = CppStringifier()
+    elif lang == "javascript":
+        func_root = _get_javascript_function_root(tree.root_node)
+        try:
+            mutable_root = JavaScriptAdaptor.convert_function_declaration(func_root)
+        except Exception as e:
+            return (False, str(e))
+        stringifier = JavaScriptStringifier()
+    else:
+        raise ValueError(f"Unsupported language: {lang}")
 
-    new_code = mutable_root.to_string()
+    new_code = stringifier.stringify(mutable_root)
     if lang == "java":
         new_code = f"public class A {{ {new_code} }}"
 
@@ -103,7 +135,9 @@ def function_round_trip(parser: tree_sitter.Parser, code: str, lang: str):
 
     old_tokens = collect_tokens(tree.root_node)
     new_tokens = collect_tokens(new_root)
-    if not compare_tokens(old_tokens, new_tokens):
+    if not compare_tokens(old_tokens, new_tokens) and lang != "javascript":
+        # NOTE: we ignore token mismatches for js
+        # because semicolons in js are optional and they cause tons of mismatches
         return (False, "token mismatch")
 
     return (True, None)
@@ -111,7 +145,7 @@ def function_round_trip(parser: tree_sitter.Parser, code: str, lang: str):
 
 def main(args):
     if len(args) != 2:
-        print("Usage: python3 dataset_filter.py <lang> <file>")
+        print("Usage: python dataset_filter.py <lang> <file>")
         return
     lang, file_path = args
 
@@ -150,6 +184,7 @@ def main(args):
         print(f"Good: {tot_good}, Fail: {tot_fail}, Total: {tot_funcs}")
         for msg, count in msg_counter.items():
             print(f"{msg}: {count}")
+    print(f"Output written to {output_file_path}")
 
 
 if __name__ == "__main__":
